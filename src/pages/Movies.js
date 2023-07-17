7df037653f79d71e657993cfa5900f5321d3f88d
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-07-17 20:41:10 +0300

    Update Movies.js

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,23 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useSearchParams, useLocation } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import MoviesList from '../components/MoviesList/MoviesList';
 import { Searchbar } from '../components/Search/search';
 import { getMoviesByQuery } from '../services/getMoviesInformation';
 
 const Movies = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [textSearch, setTextSearch] = useState('');
   const [movieForSearch, setMovieForSearch] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const location = useLocation();
   const query = searchParams.get('query');
 
-  const handleSubmit = text => {
-    setTextSearch(text);
-    setSearchParams({ query: text });
+  const handleSubmit = query => {
+    setSearchParams({ query: query });
   };
 
   useEffect(() => {
@@ -28,49 +25,20 @@ const Movies = () => {
   }, [movieForSearch]);
 
   useEffect(() => {
-    const storedMovieForSearch = localStorage.getItem(
-      `movieForSearch-${query}`
-    );
-    if (query && storedMovieForSearch) {
-      setMovieForSearch(JSON.parse(storedMovieForSearch));
-    }
-  }, [query]);
-
-  useEffect(() => {
-    if (!textSearch) {
+    if (!query) {
       return;
     }
     (async () => {
       try {
-        const data = await getMoviesByQuery(textSearch);
+        const data = await getMoviesByQuery(query);
         setMovieForSearch(data.results);
-        localStorage.setItem(
-          `movieForSearch-${textSearch}`,
-          JSON.stringify(data.results)
-        );
       } catch (error) {
         console.log(error.message);
       } finally {
         setLoading(false);
       }
     })();
-  }, [textSearch]);
-
-  useEffect(() => {
-    if (!query && location.pathname === '/movies') {
-      Object.keys(localStorage).forEach(key => {
-        if (key.includes('movieForSearch-')) {
-          localStorage.removeItem(key);
-        }
-      });
-    } else {
-      Object.keys(localStorage).forEach(key => {
-        if (key.includes('movieForSearch-') && key.split('-')[1] !== query) {
-          localStorage.removeItem(key);
-        }
-      });
-    }
-  }, [query, location]);
+  }, [query]);
 
   return (
     <>
